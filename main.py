1de91b9dd54cfd77a8826524fac3931d0d2fdf60
Github Action <action@github.com>
2022-01-28 06:13:58 +0000

    fixup: Format Python code with Black

--- main.py
+++ main.py
@@ -1,26 +1,34 @@
 from os import open
 from os import environ
 
+
 def add(a, b):
     return a + b
+
 
 def subtract(a, b):
     return a - b
 
+
 def multiply(a, b):
     return a * b
+
 
 def divide(a, b):
     return a / b
 
+
 def mod(a, b):
     return a % b
+
 
 def power(a, b):
     return a ** b
 
+
 def square(a):
     return a ** 2
+
 
 def cube(a):
     return a ** 3
