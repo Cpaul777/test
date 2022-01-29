f61a720a323b7011dfc2646685c92ee01bfd5236
m0nk3ybraindead <zyphr.euch@gmail.com>
2022-01-29 11:17:35 +0800

    test

--- main.py
+++ main.py
@@ -5,30 +5,23 @@ from os import open
 def add(a, b):
     return a + b
 
-
 def subtract(a, b):
     return a - b
-
 
 def multiply(a, b):
     return a * b
 
-
 def divide(a, b):
     return a / b
-
 
 def mod(a, b):
     return a % b
 
-
 def power(a, b):
     return a ** b
 
-
 def square(a):
     return a ** 2
-
 
 def cube(a):
     return a ** 3
